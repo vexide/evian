990c099d378be0a3fb002fc98d02fbafcdc311c2
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-10-25 11:14:28 -0500

    docs: clean up example code

--- examples/testing.rs
+++ examples/testing.rs
@@ -16,9 +16,7 @@ struct Robot {
 }
 
 impl Compete for Robot {
-    async fn autonomous(&mut self) {}
-
-    async fn driver(&mut self) {
+    async fn autonomous(&mut self) {
         let basic_motions = BasicCommands {
             linear_controller: Pid::new(0.5, 0.0, 0.0, None),
             angular_controller: Pid::new(5.0, 0.0, 0.0, None),
@@ -36,20 +34,22 @@ impl Compete for Robot {
             .await;
 
         println!("Settled");
+    }
 
-        // loop {
-        //     let left = self.controller.left_stick.y().unwrap_or_default();
-        //     let right = self.controller.right_stick.y().unwrap_or_default();
+    async fn driver(&mut self) {
+        loop {
+            let left = self.controller.left_stick.y().unwrap_or_default();
+            let right = self.controller.right_stick.y().unwrap_or_default();
 
-        //     for motor in self.left_motors.lock().await.iter_mut() {
-        //         motor.set_voltage(Motor::MAX_VOLTAGE * left).unwrap();
-        //     }
-        //     for motor in self.right_motors.lock().await.iter_mut() {
-        //         motor.set_voltage(Motor::MAX_VOLTAGE * right).unwrap();
-        //     }
+            for motor in self.left_motors.lock().await.iter_mut() {
+                motor.set_voltage(Motor::MAX_VOLTAGE * left).unwrap();
+            }
+            for motor in self.right_motors.lock().await.iter_mut() {
+                motor.set_voltage(Motor::MAX_VOLTAGE * right).unwrap();
+            }
 
-        //     sleep(Duration::from_millis(25)).await;
-        // }
+            sleep(Duration::from_millis(25)).await;
+        }
     }
 }
 
@@ -65,18 +65,6 @@ async fn main(peripherals: Peripherals) {
         Motor::new(peripherals.port_8, Gearset::Blue, Direction::Forward),
         Motor::new(peripherals.port_9, Gearset::Blue, Direction::Forward),
     ];
-
-    let mut drivetrain = DifferentialDrivetrain::new(
-        left_motors.clone(),
-        right_motors.clone(),
-        ParallelWheelTracking::new(
-            Vec2::default(),
-            90.0_f64.to_radians(),
-            TrackingWheel::new(left_motors.clone(), 3.25, 7.5, Some(36.0 / 48.0)),
-            TrackingWheel::new(right_motors.clone(), 3.25, 7.5, Some(36.0 / 48.0)),
-            None,
-        ),
-    );
 
     Robot {
         controller: peripherals.primary_controller,
--- src/differential/commands/mod.rs
+++ src/differential/commands/mod.rs
@@ -3,6 +3,8 @@
 //! This module provides implementations of various motion algorithms for differential
 //! drivetrains. Each algorithm implements the [`Command`] trait to provide voltage
 //! updates to a drivetrain until it has settled.
+//! 
+//! [`Command`]: crate::command::Command
 
 pub mod basic;
 pub mod point;
