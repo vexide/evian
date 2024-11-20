a51d94eb1b1e5b99ee4558ac263f99697bbb07d7
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-11-19 20:22:39 -0600

    docs: fix example code

--- examples/testing.rs
+++ examples/testing.rs
@@ -45,50 +45,6 @@ impl Compete for Robot {
 
 #[vexide::main]
 async fn main(peripherals: Peripherals) {
-
-    let dt = Drivetrain::new(
-        Differential::new(
-            shared_motors![
-                Motor::new(peripherals.port_2, Gearset::Blue, Direction::Reverse),
-                Motor::new(peripherals.port_3, Gearset::Blue, Direction::Reverse),
-                Motor::new(peripherals.port_7, Gearset::Blue, Direction::Reverse),
-            ],
-            shared_motors![
-                Motor::new(peripherals.port_4, Gearset::Blue, Direction::Forward),
-                Motor::new(peripherals.port_8, Gearset::Blue, Direction::Forward),
-                Motor::new(peripherals.port_9, Gearset::Blue, Direction::Forward),
-            ],
-        ),
-        ParallelWheelTracking::new(
-            Vec2::default(),
-            0.0.deg(),
-            TrackingWheel::new(
-                RotationSensor::new(peripherals.port_10, Direction::Forward),
-                3.25,
-                7.5,
-                Some(36.0 / 48.0),
-            ),
-            TrackingWheel::new(
-                RotationSensor::new(peripherals.port_11, Direction::Forward),
-                3.25,
-                7.5,
-                Some(36.0 / 48.0),
-            ),
-            None,
-        ),
-    );
-
-    let mut mtp = MoveToPoint {
-        distance_controller: Pid::new(0.5, 0.0, 0.0, None),
-        angle_controller: AngularPid::new(0.5, 0.0, 0.0, None),
-        settler: Settler::new()
-            .error_tolerance(0.3)
-            .tolerance_duration(Duration::from_millis(100))
-            .timeout(Duration::from_secs(2)),
-    };
-
-    mtp.move_to_point(dt, (24.0, 24.0)).await;
-
     Robot {
         controller: peripherals.primary_controller,
         drivetrain: Drivetrain::new(
