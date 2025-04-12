5f1473e6ec53eeb1349883bc195c2c38fc13d242
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-04-12 17:44:10 -0500

    fix: forgot to remove import

--- packages/evian-motion/src/lib.rs
+++ packages/evian-motion/src/lib.rs
@@ -3,11 +3,9 @@
 extern crate alloc;
 
 mod basic;
-mod ramsete;
 mod seeking;
 mod tolerances;
 
 pub use basic::Basic;
-pub use ramsete::Ramsete;
 pub use seeking::Seeking;
 pub use tolerances::Tolerances;
