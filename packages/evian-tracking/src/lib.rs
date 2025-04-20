fb786cbc36810602df299d02da791654b0bc7206
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-04-20 01:05:10 -0500

    docs: improve `evian_tracking` documentation

--- packages/evian-tracking/src/lib.rs
+++ packages/evian-tracking/src/lib.rs
@@ -1,4 +1,30 @@
 //! Robot localization and tracking.
+//! 
+//! This crate provides traits and systems for tracking and localizing the position, orientation,
+//! velocity, and forward travel of a mobile robot in a 2D environment. The goal is to collect and
+//! provide accurate data about the robot to motion algorithms.
+//! 
+//! The traits provided in this crate allow for motion algorithms to operate generically over multiple
+//! different implementations of tracking systems, provided the necessary data is there.
+//! 
+//! These traits include:
+//! 
+//! - [`TracksPosition`], for tracking the robot's 2D position (odometry).
+//! - [`TracksHeading`], for tracking the robot's absolute orientation (heading).
+//! - [`TracksVelocity`], for tracking the robot's linear and angular velocity.
+//! - [`TracksForwardTravel`], for tracking the robot's signed forward wheel travel.
+//! 
+//! Below is an example of a motion algorithm function that is generic across all differential drivetrains
+//! with tracking systems that measure a robot's forward wheel travel:
+//! 
+//! ```
+//! async fn motion(drivetrain: &mut Drivetrain<Differential, impl TracksForwardTravel>) {
+//!     // Motion goes here...
+//! }
+//! ```
+//! 
+//! Additionally, a reference implementation of a tracking system that performs wheeled odometry is
+//! provided by the [`wheeled`] module.
 
 #![no_std]
 
@@ -12,6 +38,16 @@ pub use sensor::RotarySensor;
 use evian_math::{Angle, Vec2};
 
 /// A tracking system that localizes a robot's 2D position.
+/// 
+/// This trait defines a system for tracking and reporting the position of a mobile robot in a
+/// 2D environment. The process of localizing a mobile robot's position is also commonly known
+/// as odometry.
+/// 
+/// # Coordinate System
+/// 
+/// Implementors of this trait MUST provide their position estimate in the [*cartesian coordinate
+/// system*](https://en.wikipedia.org/wiki/Cartesian_coordinate_system). Units are expected to be
+/// in *wheel units* — the same units that were used to measure the robot's wheel diameter.
 pub trait TracksPosition {
     /// Return's the robot's position on a 2D cartesian coordinate plane measured
     /// in wheel units.
@@ -19,22 +55,47 @@ pub trait TracksPosition {
 }
 
 /// A tracking system that tracks a robot's absolute orientation.
+/// 
+/// This trait defines a system for tracking and reporting the orientation (heading) of a mobile
+/// robot in a 2D environment. The robot's orientation refers to its direction of travel relative
+/// to a fixed reference frame. 
+/// 
+/// # Angle System
+/// 
+/// Implementors of this trait MUST provide their angles in a system compatible with cartesian
+/// coordinates in *standard position*. This means that anticlockwise motion is considered a
+/// positive rotation, and a rotation of zero degrees is inline with the x-axis.
 pub trait TracksHeading {
     /// Returns the robot's orientation bounded from [0, 2π] radians.
     fn heading(&self) -> Angle;
 }
 
 /// A tracking system that tracks a robot's linear and angular velocity.
+/// 
+/// # Units
+/// 
+/// - Linear velocity is measured in *wheel units per second*. In this instance, *wheel units*
+///   refer to whatever units the user measured their wheel diameter with.
+/// 
+/// - Angular velocity is measured in *radians per second*.
 pub trait TracksVelocity {
-    /// Returns the robot's estimated linear velocity in wheel units/sec.
+    /// Returns the robot's estimated linear velocity in wheel units per second.
     fn linear_velocity(&self) -> f64;
 
-    /// Returns the robot's estimated angular velocity in radians/sec.
+    /// Returns the robot's estimated angular velocity in radians per second.
     fn angular_velocity(&self) -> f64;
 }
 
-/// A tracking system that tracks a robot's forward wheel travel.
+/// A tracking system that tracks a robot's signed forward wheel travel.
+/// 
+/// # Units
+/// 
+/// Units are expected to be returned in *wheel units* — the same units that were used to measure the
+/// robot's wheel diameter.
 pub trait TracksForwardTravel {
-    /// Returns the average forward wheel travel of the robot in wheel units.
+    /// Returns the signed forward wheel travel of the robot in wheel units.
+    /// 
+    /// Positive values indicate forward movement from the origin of tracking, while negative
+    /// values indicate backwards movement from the origin.
     fn forward_travel(&self) -> f64;
 }
