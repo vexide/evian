904ce290f17733e0fd6c86b59dd0d5bd4e0586e8
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-04-23 02:58:42 -0500

    feat: allow cascading feedback controllers

--- packages/evian-control/src/loops/cascade.rs
+++ packages/evian-control/src/loops/cascade.rs
@@ -7,36 +7,46 @@ use super::{
 /// Combines a feedback controller with a feedforward controller by
 /// passing the feedback controller's signal into the feedforward
 /// controller's setpoint.
-pub struct Cascade<Fb: Feedback, Ff: Feedforward> {
-    /// Feedback controller.
-    pub feedback: Fb,
-
-    /// Feedforward controller.
-    pub feedforward: Ff,
+pub struct Cascade<P: ControlLoop, S: ControlLoop> {
+    pub primary: P,
+    pub secondary: S,
 }
 
-impl<Fb: Feedback<Output = f64>, Ff: Feedforward> Cascade<Fb, Ff> {
+impl<P: Feedback<Output = f64>, S: Feedforward> Cascade<P, S> {
     /// Creates a new cascading controller from a feedback and a feedforward
     /// controller.
-    pub const fn new(feedback: Fb, feedforward: Ff) -> Self {
-        Self {
-            feedback,
-            feedforward,
-        }
+    pub const fn new(primary: P, secondary: S) -> Self {
+        Self { primary, secondary }
     }
 }
 
-// MARK: MotorFeedforward
+impl<P: Feedback<Input = f64>, S: Feedback<Input = P::Output, Output = f64>> Cascade<P, S> {
+    fn update(
+        &mut self,
+        primary_measurement: P::Input,
+        secondary_measurement: S::Input,
+        setpoint: P::Input,
+        dt: Duration,
+    ) -> f64 {
+        self.secondary.update(
+            secondary_measurement,
+            self.primary.update(primary_measurement, setpoint, dt),
+            dt,
+        )
+    }
+}
 
-impl<Fb: Feedback<Output = f64>> ControlLoop for Cascade<Fb, MotorFeedforward> {
-    type Input = Fb::Input;
+// MARK: Feedback -> FeedForward
+
+impl<P: Feedback<Output = f64>> ControlLoop for Cascade<P, MotorFeedforward> {
+    type Input = P::Input;
     type Output = f64;
 }
 
-impl<Fb: Feedback<Output = f64>> Feedback for Cascade<Fb, MotorFeedforward> {
+impl<P: Feedback<Output = f64>> Feedback for Cascade<P, MotorFeedforward> {
     fn update(&mut self, measurement: Self::Input, setpoint: Self::Input, dt: Duration) -> f64 {
-        let feedback = self.feedback.update(measurement, setpoint, dt);
-        self.feedforward.update(
+        let feedback = self.primary.update(measurement, setpoint, dt);
+        self.secondary.update(
             MotorFeedforwardState {
                 velocity: feedback,
                 acceleration: 0.0,
